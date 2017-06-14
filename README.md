b784a1e5f763862522522d5da922d0ff7f0e39f5
RosalbaRCM <rosalba@oktana.io>
2017-06-14 12:16:47 -0300

    updating the readme and yarn

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# challenge
+# DonutChart
+A simple DonutChart using Reactjs and Redux.
+
+#Run
+Using yarn:
+$ yarn && yarn start
+
+Using npm:
+$ npm install
+$ npm run start
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Loading from './components/Loading';
@@ -6,8 +6,7 @@ import * as actions from './actions/actions';
 import DonutChart from './components/DonutChart';
 
 
-class App extends Component {
-  render() {
+const App = props => {
     const labels = ['Stocks', 'Bonds', 'ETFs', 'Cash', 'Commodities', 'Fixed Income'];
     const colors = ['#900', '#090', '#009', '#ff0', '#ffa500', '#0000ff'];
     return (
@@ -18,18 +17,18 @@ class App extends Component {
           </div>
           <div className="panel-body">
             <div>
-              {!this.props.slices ? <Loading /> : <DonutChart slices={this.props.slices} colors={colors} risk={this.props.risk} labels={labels} />}
+              {!props.slices ? <Loading /> : <DonutChart slices={props.slices} colors={colors} risk={props.risk} labels={labels} />}
             </div>
             <div>
               <p>Select the risk </p>
               <input
                 type="number"
-                defaultValue="1"
+                defaultValue={props.risk}
                 min="1"
                 max="10"
                 onChange={(e) => {
                   e.preventDefault();
-                  this.props.onChange(e.target.value);
+                  props.onChange(e.target.value);
                 }}
               />
             </div>
@@ -38,7 +37,7 @@ class App extends Component {
       </div>
     );
   }
-}
+
 
 App.PropTypes = {
   risk: PropTypes.string.isRequired,
